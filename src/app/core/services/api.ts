cc1057e6f26b524f86c232cd84185087f08881ca
Joaquin Hevia <joaco.r.hevia@gmail.com>
2026-04-24 12:25:52 -0300

    agrego url de la nube

--- src/app/core/services/api.ts
+++ src/app/core/services/api.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-export const API_BASE_URL = 'http://localhost:8082/api';
+//export const API_BASE_URL = 'http://localhost:8082/api';
+export const API_BASE_URL = 'https://api-tienda-production-b6fb.up.railway.app';
 
 @Injectable({
   providedIn: 'root',
